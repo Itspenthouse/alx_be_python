b2e08ceabaa46b85db69cfebb8e1e1f54c26bd71
Guzman <guzman@Guzman.Guzman>
2025-11-16 10:49:51 +0000

    fixed some bugs in that file

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,5 @@
 principal = 1000
 rate = 0.05
 time = 3
-insterest = principal * rate * time
-print("The simpel interest is:", insterest)
+interest = principal * rate * time
+print("The simple interest is:", interest)
