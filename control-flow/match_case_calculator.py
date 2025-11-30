b654d60e00f013587072dd6b4c9d36d8ed90a156
Guzman <guzman@Guzman.Guzman>
2025-11-30 17:57:05 +0000

    added a match case python file

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,19 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Choose an operation (+, -, *, /): ")
+result = None
+match operation:
+    case "+":
+        result = num1 + num2
+    case "-":
+        result = num1 - num2
+    case "*":
+        result = num1 * num2
+    case "/":
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            result = "Error: Division by zero is not allowed."
+    case _:
+        result = "Error: Invalid operation."
+print("The result is: {result}")
